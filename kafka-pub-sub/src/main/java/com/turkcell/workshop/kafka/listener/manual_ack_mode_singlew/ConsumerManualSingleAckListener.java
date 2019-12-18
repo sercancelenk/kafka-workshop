bec6755bc1ee43180b5f18e441230a014fea7a43
Sercan CELENK <sercancelenk@gmail.com>
2019-12-18 23:24:39 +0300

    Consumer with exception

--- kafka-pub-sub/src/main/java/com/turkcell/workshop/kafka/listener/manual_ack_mode_singlew/ConsumerManualSingleAckListener.java
+++ kafka-pub-sub/src/main/java/com/turkcell/workshop/kafka/listener/manual_ack_mode_singlew/ConsumerManualSingleAckListener.java
@@ -4,7 +4,6 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.listener.AcknowledgingMessageListener;
 import org.springframework.kafka.support.Acknowledgment;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
--- kafka-pub-sub/src/main/java/com/turkcell/workshop/kafka/listener/withautocommitsingle/ConsumerWithAutocommitListener.java
+++ kafka-pub-sub/src/main/java/com/turkcell/workshop/kafka/listener/withautocommitsingle/ConsumerWithAutocommitListener.java
@@ -9,7 +9,11 @@ public class ConsumerWithAutocommitListener {
 
     @KafkaListener(topics = "topic1", groupId = "ConsumerGroup1")
     public void listen(String message) {
-        System.out.println(1/0);
+        System.out.println("Received but not processed Message in group ConsumerGroup1: " + message);
+
+        if("throw".equals(message))
+            throw new RuntimeException("Throwing known exception. Kafka will be retry with limit..");
+
         System.out.println("Received Message in group ConsumerGroup1: " + message);
     }
 
